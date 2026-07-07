d7f143baf48d59a689941c98b7c8cfb494ff75d0
srinurenangi-BL <srinu.renangi@bridgelabz.com>
2026-07-07 11:26:24 +0530

    [srinu renagni] implemented extended unit support for yards and centimeters

--- src/main/java/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp.java
@@ -8,7 +8,9 @@ public class QuantityMeasurementApp {
 
     public enum LengthUnit {
         FEET(1.0),
-        INCHES(1.0 / 12.0);
+        INCHES(1.0 / 12.0),
+        YARDS(3.0),
+        CENTIMETERS(0.393701 / 12.0);
 
         private final double conversionFactorToFeet;
 
--- src/test/java/QuantityMeasurementAppTest.java
+++ src/test/java/QuantityMeasurementAppTest.java
@@ -5,6 +5,13 @@ public class QuantityMeasurementAppTest {
         testEqualityEquivalentValuesDifferentUnits();
         testEqualityFeetToFeetDifferentValue();
         testEqualityInchToInchDifferentValue();
+        testEqualityYardToYardSameValue();
+        testEqualityYardToYardDifferentValue();
+        testEqualityYardToFeetEquivalentValue();
+        testEqualityYardToInchesEquivalentValue();
+        testEqualityCentimetersToInchesEquivalentValue();
+        testEqualityCentimetersToFeetNonEquivalentValue();
+        testEqualityMultiUnitTransitiveProperty();
         testEqualityNullComparison();
         testEqualitySameReference();
         testEqualityNullUnit();
@@ -39,6 +46,51 @@ public class QuantityMeasurementAppTest {
         QuantityMeasurementApp.QuantityLength oneInch = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.INCHES);
         QuantityMeasurementApp.QuantityLength twoInches = new QuantityMeasurementApp.QuantityLength(2.0, QuantityMeasurementApp.LengthUnit.INCHES);
         assertFalse(oneInch.equals(twoInches), "Expected 1.0 inch to be different from 2.0 inches");
+    }
+
+    private static void testEqualityYardToYardSameValue() {
+        QuantityMeasurementApp.QuantityLength oneYard = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.YARDS);
+        QuantityMeasurementApp.QuantityLength anotherYard = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.YARDS);
+        assertTrue(oneYard.equals(anotherYard), "Expected 1.0 yard to be equal to 1.0 yard");
+    }
+
+    private static void testEqualityYardToYardDifferentValue() {
+        QuantityMeasurementApp.QuantityLength oneYard = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.YARDS);
+        QuantityMeasurementApp.QuantityLength twoYards = new QuantityMeasurementApp.QuantityLength(2.0, QuantityMeasurementApp.LengthUnit.YARDS);
+        assertFalse(oneYard.equals(twoYards), "Expected 1.0 yard to be different from 2.0 yards");
+    }
+
+    private static void testEqualityYardToFeetEquivalentValue() {
+        QuantityMeasurementApp.QuantityLength oneYard = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.YARDS);
+        QuantityMeasurementApp.QuantityLength threeFeet = new QuantityMeasurementApp.QuantityLength(3.0, QuantityMeasurementApp.LengthUnit.FEET);
+        assertTrue(oneYard.equals(threeFeet), "Expected 1 yard to equal 3 feet");
+    }
+
+    private static void testEqualityYardToInchesEquivalentValue() {
+        QuantityMeasurementApp.QuantityLength oneYard = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.YARDS);
+        QuantityMeasurementApp.QuantityLength thirtySixInches = new QuantityMeasurementApp.QuantityLength(36.0, QuantityMeasurementApp.LengthUnit.INCHES);
+        assertTrue(oneYard.equals(thirtySixInches), "Expected 1 yard to equal 36 inches");
+    }
+
+    private static void testEqualityCentimetersToInchesEquivalentValue() {
+        QuantityMeasurementApp.QuantityLength oneCentimeter = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.CENTIMETERS);
+        QuantityMeasurementApp.QuantityLength expectedInches = new QuantityMeasurementApp.QuantityLength(0.393701, QuantityMeasurementApp.LengthUnit.INCHES);
+        assertTrue(oneCentimeter.equals(expectedInches), "Expected 1 centimeter to equal 0.393701 inches");
+    }
+
+    private static void testEqualityCentimetersToFeetNonEquivalentValue() {
+        QuantityMeasurementApp.QuantityLength oneCentimeter = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.CENTIMETERS);
+        QuantityMeasurementApp.QuantityLength oneFoot = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.FEET);
+        assertFalse(oneCentimeter.equals(oneFoot), "Expected 1 centimeter to be different from 1 foot");
+    }
+
+    private static void testEqualityMultiUnitTransitiveProperty() {
+        QuantityMeasurementApp.QuantityLength oneYard = new QuantityMeasurementApp.QuantityLength(1.0, QuantityMeasurementApp.LengthUnit.YARDS);
+        QuantityMeasurementApp.QuantityLength threeFeet = new QuantityMeasurementApp.QuantityLength(3.0, QuantityMeasurementApp.LengthUnit.FEET);
+        QuantityMeasurementApp.QuantityLength thirtySixInches = new QuantityMeasurementApp.QuantityLength(36.0, QuantityMeasurementApp.LengthUnit.INCHES);
+        assertTrue(oneYard.equals(threeFeet), "Expected 1 yard to equal 3 feet");
+        assertTrue(threeFeet.equals(thirtySixInches), "Expected 3 feet to equal 36 inches");
+        assertTrue(oneYard.equals(thirtySixInches), "Expected 1 yard to equal 36 inches transitively");
     }
 
     private static void testEqualityNullComparison() {
