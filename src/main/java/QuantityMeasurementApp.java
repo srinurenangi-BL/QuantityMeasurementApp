2b01d6a0caa82d6e995c983c1f0737ca38106fa2
srinurenangi-BL <srinu.renangi@bridgelabz.com>
2026-07-07 11:19:48 +0530

    [srinu renagni] implemented usecase_details

--- src/main/java/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp.java
@@ -1,9 +1,22 @@
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        Feet oneFoot = new Feet(1.0);
-        Feet anotherFoot = new Feet(1.0);
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + areInchesEqual(1.0, 1.0) + ")");
+
         System.out.println("Input: 1.0 ft and 1.0 ft");
-        System.out.println("Output: Equal (" + oneFoot.equals(anotherFoot) + ")");
+        System.out.println("Output: Equal (" + areFeetEqual(1.0, 1.0) + ")");
+    }
+
+    public static boolean areFeetEqual(double firstValue, double secondValue) {
+        Feet firstFoot = new Feet(firstValue);
+        Feet secondFoot = new Feet(secondValue);
+        return firstFoot.equals(secondFoot);
+    }
+
+    public static boolean areInchesEqual(double firstValue, double secondValue) {
+        Inches firstInch = new Inches(firstValue);
+        Inches secondInch = new Inches(secondValue);
+        return firstInch.equals(secondInch);
     }
 
     public static class Feet {
@@ -22,6 +35,26 @@ public class QuantityMeasurementApp {
                 return false;
             }
             Feet other = (Feet) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+            Inches other = (Inches) obj;
             return Double.compare(this.value, other.value) == 0;
         }
     }
--- src/test/java/QuantityMeasurementAppTest.java
+++ src/test/java/QuantityMeasurementAppTest.java
@@ -1,38 +1,71 @@
 public class QuantityMeasurementAppTest {
     public static void main(String[] args) {
-        testEqualitySameValue();
-        testEqualityDifferentValue();
-        testEqualityNullComparison();
-        testEqualityDifferentType();
-        testEqualitySameReference();
+        testFeetEqualitySameValue();
+        testFeetEqualityDifferentValue();
+        testFeetEqualityNullComparison();
+        testFeetEqualityDifferentType();
+        testFeetEqualitySameReference();
+
+        testInchesEqualitySameValue();
+        testInchesEqualityDifferentValue();
+        testInchesEqualityNullComparison();
+        testInchesEqualityDifferentType();
+        testInchesEqualitySameReference();
         System.out.println("All tests passed.");
     }
 
-    private static void testEqualitySameValue() {
+    private static void testFeetEqualitySameValue() {
         QuantityMeasurementApp.Feet oneFoot = new QuantityMeasurementApp.Feet(1.0);
         QuantityMeasurementApp.Feet anotherFoot = new QuantityMeasurementApp.Feet(1.0);
         assertTrue(oneFoot.equals(anotherFoot), "Expected 1.0 ft to be equal to 1.0 ft");
     }
 
-    private static void testEqualityDifferentValue() {
+    private static void testFeetEqualityDifferentValue() {
         QuantityMeasurementApp.Feet oneFoot = new QuantityMeasurementApp.Feet(1.0);
         QuantityMeasurementApp.Feet twoFeet = new QuantityMeasurementApp.Feet(2.0);
         assertFalse(oneFoot.equals(twoFeet), "Expected 1.0 ft to be different from 2.0 ft");
     }
 
-    private static void testEqualityNullComparison() {
+    private static void testFeetEqualityNullComparison() {
         QuantityMeasurementApp.Feet oneFoot = new QuantityMeasurementApp.Feet(1.0);
         assertFalse(oneFoot.equals(null), "Expected comparison with null to return false");
     }
 
-    private static void testEqualityDifferentType() {
+    private static void testFeetEqualityDifferentType() {
         QuantityMeasurementApp.Feet oneFoot = new QuantityMeasurementApp.Feet(1.0);
         assertFalse(oneFoot.equals("not-a-feet-value"), "Expected comparison with a different type to return false");
     }
 
-    private static void testEqualitySameReference() {
+    private static void testFeetEqualitySameReference() {
         QuantityMeasurementApp.Feet oneFoot = new QuantityMeasurementApp.Feet(1.0);
         assertTrue(oneFoot.equals(oneFoot), "Expected an object to be equal to itself");
+    }
+
+    private static void testInchesEqualitySameValue() {
+        QuantityMeasurementApp.Inches oneInch = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches anotherInch = new QuantityMeasurementApp.Inches(1.0);
+        assertTrue(oneInch.equals(anotherInch), "Expected 1.0 inch to be equal to 1.0 inch");
+    }
+
+    private static void testInchesEqualityDifferentValue() {
+        QuantityMeasurementApp.Inches oneInch = new QuantityMeasurementApp.Inches(1.0);
+        QuantityMeasurementApp.Inches twoInches = new QuantityMeasurementApp.Inches(2.0);
+        assertFalse(oneInch.equals(twoInches), "Expected 1.0 inch to be different from 2.0 inches");
+    }
+
+    private static void testInchesEqualityNullComparison() {
+        QuantityMeasurementApp.Inches oneInch = new QuantityMeasurementApp.Inches(1.0);
+        assertFalse(oneInch.equals(null), "Expected comparison with null to return false");
+    }
+
+    private static void testInchesEqualityDifferentType() {
+        QuantityMeasurementApp.Inches oneInch = new QuantityMeasurementApp.Inches(1.0);
+        assertFalse(oneInch.equals("not-an-inch-value"), "Expected comparison with a different type to return false");
+    }
+
+    private static void testInchesEqualitySameReference() {
+        QuantityMeasurementApp.Inches oneInch = new QuantityMeasurementApp.Inches(1.0);
+        assertTrue(oneInch.equals(oneInch), "Expected an object to be equal to itself");
     }
 
     private static void assertTrue(boolean condition, String message) {
